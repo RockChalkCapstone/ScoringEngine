ccf9a143112e64369fee3c9ec4a4b0b2110588d6
awarrington0895 <awarrington0895@gmail.com>
2017-10-24 15:49:58 -0400

    Finishes code and adds comments

--- backend.py
+++ backend.py
@@ -1,11 +1,19 @@
 import pwd
 
 
-
-
-try:
-  pwd.getpwnam('fack')
-except KeyError:
-  print('User someusr does not exist')
+def check_user_exists(userName):
+  """
+    Function checks if user exists on the machine
+    @params:
+      userName {string}: Username of user being checked for existence
+  """
+  try:
+    # Searches /etc/passwd for entry with username userName
+    pwd.getpwnam(userName)
+    print("User '%s' exists" % (userName))
+  except KeyError:
+    print("User '%s' does not exist" % (userName))
   
- 
+
+
+  

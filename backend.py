d6a7c1765d380968ccc0f8cda68c158e28ab4f86
awarrington0895 <awarrington0895@gmail.com>
2017-10-24 21:45:58 -0400

    Changes function definition and adds return code

--- backend.py
+++ backend.py
@@ -1,31 +1,27 @@
 import pwd
 
 
-def check_user_exists(userName):
-  """
+def check_user_exists(userName: str) -> bool:
+    """
     Checks if user exists on the machine
-    
+
     Parameters
     ----------
     userName : str
-      Username of user being checked for existence
-      
+        Username of user being checked for existence
+
     Returns
     -------
-    int
-      0 if user exists
-      1 if user does not exist
-      
-  """
-  try:
+    bool
+        True if user exists
+        False if user does not exist
+
+    """
+    try:
     # Searches /etc/passwd for entry with username userName
-    pwd.getpwnam(userName)
-    print("User '%s' exists" % (userName))
-    return 0
-  except KeyError:
-    print("User '%s' does not exist" % (userName))
-    return 1
-  
-
-
-check_user_exists('test')
+        pwd.getpwnam(userName)
+        print(True)
+        return True
+    except KeyError:
+        print(False)
+        return False

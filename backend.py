9f01a9b7542958bcdfd8a78fc766df54cfcc6e4f
awarrington0895 <awarrington0895@gmail.com>
2017-10-24 20:05:21 -0400

    Changes documentation to conform to standards

--- backend.py
+++ backend.py
@@ -3,17 +3,29 @@ import pwd
 
 def check_user_exists(userName):
   """
-    Function checks if user exists on the machine
-    @params:
-      userName {string}: Username of user being checked for existence
+    Checks if user exists on the machine
+    
+    Parameters
+    ----------
+    userName : str
+      Username of user being checked for existence
+      
+    Returns
+    -------
+    int
+      0 if user exists
+      1 if user does not exist
+      
   """
   try:
     # Searches /etc/passwd for entry with username userName
     pwd.getpwnam(userName)
     print("User '%s' exists" % (userName))
+    return 0
   except KeyError:
     print("User '%s' does not exist" % (userName))
+    return 1
   
 
 
-  
+check_user_exists('test')

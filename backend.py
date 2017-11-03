2a728aa6a3577c0cde865a4ef89d1d0bb4124bd9
Alex <awarrington0895@gmail.com>
2017-11-02 21:06:27 -0500

    Adds docstring for function

---
+++ backend.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def check_sudo_user_password():
+    """
+    Function checks if sudo users are set to require passwords
+    
+    Parameters
+    ----------
+    None
+       
+    Returns
+    -------
+    bool
+        True if sudo users must use passwords to execute sudo commands
+        False if sudo users do not have to use passwords to execute sudo commands
+        
+    """
+        
+    
+    
+    

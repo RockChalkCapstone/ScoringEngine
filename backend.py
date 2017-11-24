a7c0e2aa52c1b224bce283a7af1c7e22c12b92d1
Alex <awarrington0895@gmail.com>
2017-11-24 03:59:25 +0000

    Adds function documentation

--- backend.py
+++ backend.py
@@ -2,17 +2,42 @@
 
 import os
         
-fileList = []
-filename = "init"
-
-for entry in os.scandir("/etc/pam.d"):
-   if not entry.name.startswith('.') and entry.is_file():
-       fileList.append(entry.name)
-
-if "common-password" in fileList:
-  filename = "/etc/pam.d/common-password"
-  
-print(filename)
+def check_password_policy(changeTries=None, maxDays=None, maxLen=None, minLen=None) -> bool:
+    """
+    Checks if passwords policy is enforced
+    
+    Scrubs the /etc/login.defs file for parameters:
+    [
+        PASS_CHANGE_TRIES, PASS_MAX_DAYS, 
+        PASS_MAX_LEN, PASS_MIN_LEN
+    ]
+    
+    User can set values for these parameters to match
+    
+    Parameters
+    ----------
+    
+    changeTries : int
+        DEFAULT = None
+        Represents how many times a user may attempt to change passwored
+        before they are locked out
+    maxDays : int
+        DEFAULT = None
+        Represents maximum days that a password will stay valid
+    maxLen : int    
+        DEFAULT = None
+        Represents maximum length of new passwords
+    minLen : int
+        DEFAULT = None
+        Represents minimum length of new passwords
+        
+    Returns
+    -------
+    
+    bool
+        True if parameters set by user match what is in /etc/login.defs
+        False otherwise
+        
        
+    """
        
-# password [success=2 default=ignore] pam_unix.so obscure sha512 minlen=8
